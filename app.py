d87d1739ac41ad367471ecc672ef086a30850daa
Amogh Suman <amoghsuman@gmail.com>
2025-04-17 11:56:58 +0530

    fix: use Render's port for Gradio

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from pdf_text_extractor import interface
+import os
 
 if __name__ == "__main__":
-    interface.launch()
+    port = int(os.environ.get("PORT", 7860))
+    interface.launch(server_name="0.0.0.0", server_port=port)
